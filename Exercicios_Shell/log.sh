2ff5515b839d2b519c0f90fa3ef0e3f59ef0d161
maiataniguchi <maaymt@gmail.com>
2022-10-13 20:08:00 -0300

    Exercicios shell

---
+++ Exercicios_Shell/log.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Seu diretório atual é:"
+pwd
+
+echo "Maiara Mitiko Taniguchi, 31 anos"
+
---
+++ Exercicios_Shell/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "a. O diretório corrente:"
+pwd
+
+echo "b. Nome completo e idade: Maiara Mitiko Taniguchi, 32 anos"
+
+echo "c. Processos em execução "
+ps -u
+
+echo "d. Uma frase importante: Que a força esteja com você"
